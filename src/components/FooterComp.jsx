29283edc53df8c6ac92f9df84333c3290357c229
FranDelgado20 <delgadofran78@gmail.com>
2024-02-23 19:48:36 -0300

    agrego navbar y footer

--- src/components/FooterComp.jsx
+++ src/components/FooterComp.jsx
@@ -1,7 +1,33 @@
+import { Col, Container, Row } from "react-bootstrap";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const FooterComp = () => {
-  return <div></div>;
+  return (
+    <Container fluid className="color-nav-footer py-2 ">
+      <Row className=" text-neutral-100 align-items-center ">
+        <Col sm={6} className="flex justify-center my-3">
+          <Link to={"/"}>
+            <img src="/logo.png" alt="logo" className="w-48" />
+          </Link>
+        </Col>
+        <Col sm={6} className="flex justify-center my-3 align-items-baseline">
+          <h3>Encontranos en</h3>
+          <a
+            href="https://www.linkedin.com/in/jbd-devs-87426a2b4/"
+            className="linkedIn"
+            target="_blank"
+          >
+            <i className="bi bi-linkedin fs-3 mx-3 "></i>
+          </a>
+        </Col>
+      </Row>
+      <hr className="text-neutral-100" />
+      <p className="text-center text-neutral-100">
+        © 2024 Deco Motivo. All rights reserved.
+      </p>
+    </Container>
+  );
 };
 
 export default FooterComp;
--- src/components/NavbarComp.jsx
+++ src/components/NavbarComp.jsx
@@ -1,7 +1,132 @@
-import React from "react";
-
+import React, { Fragment } from "react";
+import { useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { Link, useLocation } from "react-router-dom";
+const navigation = [
+  { name: "Inicio", to: "/", current: false },
+  { name: "Productos", to: "/products", current: false },
+  { name: "Contacto", to: "/contact", current: false },
+];
 const NavbarComp = () => {
-  return <div></div>;
+  const { pathname } = useLocation();
+
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const handleClick = () => setMobileMenuOpen(!mobileMenuOpen);
+  return (
+    <>
+      <header className=" color-nav-footer">
+        <nav
+          className="flex items-center justify-between p-1 lg:px-8"
+          aria-label="Global"
+        >
+          <div className="flex lg:flex-1">
+            <Link to={"/"} className="-m-1.5 p-1.5 w-36">
+              {/* <span className="sr-only">Your Company</span> */}
+              <img className="img-fluid " src="/logo.png" alt="Logo JDB Devs" />
+            </Link>
+          </div>
+          <div className="flex lg:hidden">
+            <button
+              type="button"
+              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+              onClick={() => setMobileMenuOpen(true)}
+            >
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon className="h-6 w-6 " aria-hidden="true" />
+            </button>
+          </div>
+          <div className="hidden lg:flex lg:gap-x-12">
+            {navigation.map((item) => (
+              <Link
+                key={item.name}
+                to={item.to}
+                className={`text-sm font-semibold leading-6 px-2 py-1 text-md no-underline rounded-md ${
+                  pathname === item.to
+                    ? "text-white bg-boton-nav"
+                    : "text-neutral-100 hover:bg-red-600  hover:text-white"
+                }`}
+              >
+                {item.name}
+              </Link>
+            ))}
+          </div>
+          <div className="hidden lg:flex lg:flex-1 lg:justify-end"></div>
+        </nav>
+        <Transition.Root as={Fragment} show={mobileMenuOpen}>
+          <Dialog
+            as="div"
+            className="lg:hidden"
+            open={mobileMenuOpen}
+            onClose={setMobileMenuOpen}
+          >
+            <Transition.Child
+              as={Fragment}
+              enter="transition-opacity ease-linear duration-500"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="transition-opacity ease-linear duration-500"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div className="fixed inset-0 z-50" />
+            </Transition.Child>
+            <Transition.Child
+              as={Fragment}
+              enter="transition ease-in-out duration-500 transform"
+              enterFrom="-translate-y-full"
+              enterTo="translate-y-0"
+              leave="transition ease-in-out duration-500 transform"
+              leaveFrom="translate-y-0"
+              leaveTo="-translate-y-full"
+            >
+              <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-neutral-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                <div className="flex items-center justify-between">
+                  <Link to="/" className="-m-1.5 p-1.5 w-44">
+                    {/* <span className="sr-only">Your Company</span> */}
+                    <img
+                      className="img-fluid"
+                      src="11-removebg-preview.png"
+                      alt="Logo JDB Devs"
+                    />
+                  </Link>
+                  <button
+                    type="button"
+                    className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                    onClick={() => setMobileMenuOpen(false)}
+                  >
+                    <span className="sr-only">Close menu</span>
+                    <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                  </button>
+                </div>
+                <div className="mt-6 flow-root">
+                  <div className="-my-6 divide-y divide-gray-500/10">
+                    <div className="space-y-2 py-6">
+                      {navigation.map((item) => (
+                        <Link
+                          key={item.name}
+                          to={item.to}
+                          onClick={handleClick}
+                          className={`block text-base font-semibold leading-7 text-neutral-100 hover:bg-neutral-700 hover:text-white rounded-md px-2 py-1 text-md  no-underline ${
+                            pathname === item.to
+                              ? "text-white bg-stone-800"
+                              : "text-neutral-100 hover:bg-neutral-700 hover:text-white"
+                          }`}
+                        >
+                          {item.name}
+                        </Link>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </Dialog>
+        </Transition.Root>
+      </header>
+    </>
+  );
 };
 
 export default NavbarComp;
