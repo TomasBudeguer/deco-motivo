7a5980fd3677dcbab5724ce2a581aeaf708462ca
FranDelgado20 <delgadofran78@gmail.com>
2024-02-23 21:37:38 -0300

    agrego pagina de contacto

--- src/routes/RoutesViews.jsx
+++ src/routes/RoutesViews.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Error404 from "../pages/Error404";
 import HomePage from "../pages/HomePage";
+import ContactPage from "../pages/ContactPage";
 
 const RoutesViews = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/contact" element={<ContactPage />} />
       <Route path="*" element={<Error404 />} />
     </Routes>
   );
